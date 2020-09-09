5888ad21977ece121395e0be2221a3755a35ef99
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-09-09 00:10:42 -0300

    filtrando transactions pela descricao e reagindo certo na tela. O botão de anterior e proximo periodo para filtro do select tambem está funcionando

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import Container from './components/Container';
+import Dashboard from './components/Dashboard';
 
 export default function App() {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <header>
-        <h1>Bootcamp Full Stack - Desafio Final</h1>
-      </header>
-      <Container />
-    </div>
-  );
+  return <Dashboard />;
 }
