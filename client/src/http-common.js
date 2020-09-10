94c5c81fc2d553cd6acf2219f8a644815945fcda
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-09-10 18:56:46 -0300

    alterada rota de localhost para heroku

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://hochscheidt-desafio-final.herokuapp.com/api',
   headers: {
     'Content-type': 'application/json',
   },
